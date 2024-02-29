7e9a36f2631d89785f45c6bef8323dbe70ed663f
SummerChalmers <159860810+SummerChalmers@users.noreply.github.com>
2024-02-28 19:02:15 -0600

    Add files via upload

--- TruckerClassWork.R
+++ TruckerClassWork.R
@@ -32,11 +32,36 @@ print(average_per_day_rec)
 df$fuel_cost <- df$Gallons * df$Price.per.Gallon
 total_fuel_cost <- round(sum(df$Gallons * df$Price.per.Gallon) , 2)
 
-df[c('warehouse' ,'city_state')] <- str_split_fixed(df$Starting.Location, ',',2) 
+df[c('warehouse' ,'starting_city_state')] <- str_split_fixed(df$Starting.Location, ',',2) 
 
 total_cost <- round(total_fuel_cost + sum(df$Misc) +sum(df$Tolls), 2)
 
 total_gallons_consumed <- sum(df$Gallons)
 total_miles_driven <- sum(df$Odometer.Ending- df$Odometer.Beginning)
 miles_per_gallon <- round(total_miles_driven / total_gallons_consumed ,2)
-total_cost_per_mile <- total_cost_per_mile * 
+#total_cost_per_mile <- total_cost_per_mile * 
+
+df$city_state <- gsub(',', "", df$starting_city_state)
+#df[c('col1' ,'col2')] <- str_split_fixed(df$city_state, ' ',2)
+
+df_starting_pivot <- df %>%
+  group_by(starting_city_state) %>%
+  summarize(count = n(),
+            mean_size_hours = mean(Hours, na.rm = TRUE),
+            sd_hours = sd(Hours, na.rm = TRUE),
+            total_hours = sum(Hours, na.rm =TRUE),
+            total_gallons = sum(Gallons, na.rm = TRUE)
+            )
+
+ggplot(df_starting_pivot, aes(x = starting_city_state, y =count)) +
+  geom_col() +
+  theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1))
+
+df$delivery_location <- gsub(',', "", df$delivery_location)
+df_delivery_location_pivot <- df %>%
+  group_by(Delivery.Location) %>%
+  summarize(count = n())
+ggplot(df_delivery_location_pivot, aes(x = Delivery.Location, y =count)) +
+  geom_col() +
+  theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1))
+

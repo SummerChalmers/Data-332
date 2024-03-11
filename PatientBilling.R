d53900de844d43fdfabb1cbf982a9d29f741073d
SummerChalmers <159860810+SummerChalmers@users.noreply.github.com>
2024-03-10 20:12:25 -0500

    Update PatientBilling.R

--- PatientBilling.R
+++ PatientBilling.R
@@ -5,9 +5,9 @@ library(ggplot2)
 library(dplyr)
 
 setwd('H:/Data332/PatientBilling')
-df_billing <- readxl::read_excel('Billing.xlsx')
-df_patient <- readxl::read_excel('Patient.xlsx')
-df_visit <- readxl::read_excel('Visit.xlsx')
+df_billing <- readxl::read_excel('Billing.xlsx', .name_repair = 'universal')
+df_patient <- readxl::read_excel('Patient.xlsx', .name_repair = 'universal')
+df_visit <- readxl::read_excel('Visit.xlsx', .name_repair = 'universal')
 
 df <- left_join(df_visit, df_patient, by = c('PatientID'))
 df <- left_join(df, df_billing, by = c('VisitID'))
@@ -42,13 +42,17 @@ ggplot(df_reason_by_zip, aes(x = Zip, y= count))+
 
 df_invoice_by_reason <- df %>%
   group_by(Reason) %>%
-  summarize(count = sum(invoice))
+  summarize(count = sum(InvoiceAmt))
 ggplot(df_invoice_by_reaon, aes(x = reason, y = count))+
 geom_col()+
 theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1))
 
+df$VisitDate <- as.Date(df$VisitDate)
+df$VisitYear <- year(df$VisitDate)
+df$VisitMonth <- month(df$VisitDate)
+
 df_reason_by_month <- df %>%
-  group_by(Month) %>%
+  group_by(df$VisitMonth) %>%
   summarize(count = sum(Reason))
 ggplot(df_reason_by_month, aes(x = reason, y = count),col =c("red", "blue"))+
 geom_col()+

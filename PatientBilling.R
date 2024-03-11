e43d10312a238ca376dcc04980f455df8e26ef8e
SummerChalmers <159860810+SummerChalmers@users.noreply.github.com>
2024-03-10 20:47:55 -0500

    Update PatientBilling.R

--- PatientBilling.R
+++ PatientBilling.R
@@ -40,12 +40,18 @@ ggplot(df_reason_by_zip, aes(x = Zip, y= count))+
   geom_col()+
   theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1))
 
+df$ShortReason <- sapply(strsplit(as.character(df$Reason), " "), function(x) x[1])
+                
 df_invoice_by_reason <- df %>%
   group_by(Reason) %>%
-  summarize(count = sum(InvoiceAmt))
-ggplot(df_invoice_by_reaon, aes(x = reason, y = count))+
+  summarize(total_invoice = sum(InvoiceAmt))
+ggplot(df_invoice_by_reaon, aes(x = total_invoice, y = ShortReason, fill = InvoicePaid))+
 geom_col()+
-theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1))
+                         labs(title = "Total Invoice Amount",
+                              x = "Cost"
+                              y= "Reason"
+                              fill = "Paid")+
+                         theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1))
 
 df$VisitDate <- as.Date(df$VisitDate)
 df$VisitYear <- year(df$VisitDate)
@@ -54,7 +60,12 @@ df$VisitMonth <- month(df$VisitDate)
 df_reason_by_month <- df %>%
   group_by(df$VisitMonth) %>%
   summarize(count = sum(Reason))
-ggplot(df_reason_by_month, aes(x = reason, y = count),col =c("red", "blue"))+
+ggplot(df_reason_by_month, aes(x = Reason, y = count),col =c("red", "blue"))+
 geom_col()+
+                         labs(title = "Reasons In Each Month",
+                              x = 'count'
+                              y = "Month"
+                              fill = "Reason")
+scale_x_continous(breaks = 1:12, labels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))+
 theme(axis.text = element_text(angle = 45, vjust = .5, hjust = 1)
 
